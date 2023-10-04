2d26bce697d315bffb682eeeca2eb154938f40bb
Raman Dahiya <xdahiya@gmail.com>
2023-10-04 17:02:03 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,31 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import logger from "./config/logger";
+import createError, { HttpError } from "http-errors";
+
 const app = express();
 
 app.get("/", (req, res) => {
+    const err = createError(401, "manthan not allowed to view this page");
+    throw err;
     res.json({ msg: "done" });
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                location: "",
+                path: "",
+            },
+        ],
+    });
 });
 
 export default app;
