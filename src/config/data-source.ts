e0da5b2f66ade4e71082011bebd26c67b747ffaf
Raman Dahiya <xdahiya@gmail.com>
2023-10-21 15:14:39 +0530

    manual test insomnia

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     username: Config.DB_USERNAME,
     password: Config.DB_PASSWORD,
     database: Config.DB_NAME,
-    synchronize: false,
+    synchronize: true,
     // synchronize: Config.NODE_ENV == "dev" || Config.NODE_ENV == "test",
     logging: false,
     entities: [User, RefreshToken],
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from "./app";
 import { Config } from "./config";
+import { AppDataSource } from "./config/data-source";
 import logger from "./config/logger";
 
-const startServer = () => {
+const startServer = async () => {
     try {
+        await AppDataSource.initialize();
         const PORT = Config.PORT;
+        logger.info("DATABASE CONNECTED SUCCESSFULLY ");
         app.listen(PORT, () => {
             logger.info(`START LISTENTING ON PORT : ${PORT}`);
         });
@@ -18,4 +21,10 @@ const startServer = () => {
     }
 };
 
-startServer();
+startServer()
+    .then(() => {
+        logger.info("STARTED");
+    })
+    .catch((err) => {
+        logger.error("ERROR :", err);
+    });
